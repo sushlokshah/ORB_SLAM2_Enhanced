c9b1aed4fc22e5cf18c3b8a5daf8677c11bb5d6e
sushlokshah <sushlokshahkop9@gmail.com>
2021-08-07 22:05:36 +0530

    added file to convert pointcloud data to ply file

---
+++ txt_to_ply.py
@@ -0,0 +1,43 @@
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+from os import listdir
+import numpy as np
+import cv2 as cv
+import time
+import struct
+import yaml
+
+def write_pointcloud(filename,xyz_points,rgb_points=None):
+
+    """ creates a .pkl file of the point clouds generated
+    """
+
+    assert xyz_points.shape[1] == 3,'Input XYZ points should be Nx3 float array'
+    if rgb_points is None:
+        rgb_points = np.ones(xyz_points.shape).astype(np.uint8)*255
+    assert xyz_points.shape == rgb_points.shape,'Input RGB colors should be Nx3 float array and have same size as input XYZ points'
+    # cleaning point cloud
+    # Write header of .ply file
+    fid = open(filename,'wb')
+    fid.write(bytes('ply\n', 'utf-8'))
+    fid.write(bytes('format binary_little_endian 1.0\n', 'utf-8'))
+    fid.write(bytes('element vertex %d\n'%xyz_points.shape[0], 'utf-8'))
+    fid.write(bytes('property float x\n', 'utf-8'))
+    fid.write(bytes('property float y\n', 'utf-8'))
+    fid.write(bytes('property float z\n', 'utf-8'))
+    fid.write(bytes('property uchar red\n', 'utf-8'))
+    fid.write(bytes('property uchar green\n', 'utf-8'))
+    fid.write(bytes('property uchar blue\n', 'utf-8'))
+    fid.write(bytes('end_header\n', 'utf-8'))
+
+    # Write 3D points to .ply file
+    for i in range(xyz_points.shape[0]):
+        fid.write(bytearray(struct.pack("fffccc",xyz_points[i,0],xyz_points[i,1],xyz_points[i,2],
+                                        rgb_points[i,2].tobytes(),rgb_points[i,1].tobytes(),
+                                        rgb_points[i,0].tobytes())))
+    fid.close()
+
+a = np.loadtxt("pose1.txt",delimiter = "  ")
+print(a.shape)
+write_pointcloud("output.ply",a)
